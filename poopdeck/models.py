a90caeaa3e15c7550631af78781b84578fa554ed
cbanks0508 <35264440+cbanks0508@users.noreply.github.com>
2018-04-03 21:51:28 -0400

    Added meals class

--- poopdeck/models.py
+++ poopdeck/models.py
@@ -11,3 +11,12 @@ class Events(models.Model):
 		return self.last + "," + self.first
 
 
+class Meals(models.Model):
+        TypeofMeal = models.CharField(max_length=20)
+        SpecialOccasion = models.CharField(max_length=30)
+	Location = models.CharField(max_length=30)
+	DurationRange = models.CharField(max_length=30)
+	AnnouncementsMade = models.CharField(max_length=200)
+	def __str__(self):
+		return self.TypeofMeal + "," + self.SpecialOccasion + "," + self.Location + "," + self.DurationRange + "," + self.AnnouncementsMade
+	
