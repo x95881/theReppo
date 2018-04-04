ad7f4dbf7f6ec4dfbbe33406ef58c5b651803f15
x95881 <35264586+x95881@users.noreply.github.com>
2018-04-03 21:46:44 -0400

    Delete Cadet Class

--- poopdeck/models.py
+++ poopdeck/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-class Cadet(models.Model):
+
+class Events(models.Model):
 	first = models.CharField(max_length=200)
 	last = models.CharField(max_length=200)
 	xnumber = models.CharField(max_length=8)
 	def __str__(self):
 		return self.last + "," + self.first
+
 
