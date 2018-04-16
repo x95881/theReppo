e0ee4fc6c3412e605700cbd7b94ad9806a301d2d
niv203 <35264438+niv203@users.noreply.github.com>
2018-04-16 09:04:16 -0400

    Update tests.py

--- poopdeck/tests.py
+++ poopdeck/tests.py
@@ -1,6 +1,23 @@
 from django.test import TestCase
+from poopdeck.models import *
 
 # Create your tests here.
 def index(request):
         return HttpResponse("Hello, world. You're at the Reppo index.")
 
+#####################################################
+
+class EventsTestCase(TestCase):
+        def setUp(self):
+                Event.objects.create(
+                        IDnumber = 100001,
+                        Description = "test test test",
+                        DurationRange = "100001",
+                        ImportanceStatus = "100001",
+                        Location = "test location",
+                        CompletionStatus = "testing Testing" 
+                )
+        def test_event_was_made(self):
+                # checks if data can be retreived 
+                test_event = Event.objects.get(IDnumber = 100001)
+                test_event
