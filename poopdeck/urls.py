bf6ddcc953ad7886a5b1a0eaaef43d5f705e2cbc
Banks <x90478@usmawkddth17610.eecs.usma.edu>
2018-04-16 09:08:56 -0400

    Added admin

--- poopdeck/urls.py
+++ poopdeck/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('accounts/', include('django.contrib.auth.urls')),
+	#path('<int:cgr_id>/', views.cgr, name='cgr'),
+	#path('<int:cgr_id>/update', views.update, name='update'),
 ]
 
